8c2c233f0d4cfb37ca36bad5da11fc9dfa157106
Kedus Meheretu <kedusmeheretu@Keduss-MacBook-Pro.local>
2022-05-15 12:13:00 -0400

    second commit

--- 7kyu/Highest and Lowest.js
+++ 7kyu/Highest and Lowest.js
@@ -1,10 +1,10 @@
 // In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
 
 function highestAndLowest(numbers){
-  numbers = numbers.split(" ");
+  numbers = numbers.split(" ")
   return Math.max.apply(null, numbers) + " " +  Math.min.apply(null, numbers)
 }
 
 
 // test
-console.log(highestAndLowest("1 2 3 4 5"));
+console.log(highestAndLowest("1 2 3 4 5"))
