896446a3c7165f74b54168347db0efb7875f8b5f
Kedus Meheretu <kedusmeheretu@Keduss-MacBook-Pro.local>
2022-07-08 17:15:53 -0400

    first commit

---
+++ 7kyu/Filter Strings.js
@@ -0,0 +1,15 @@
+// In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+function filter_list(arr) {
+  // Return a new array with the strings filtered out
+  const noStringsArr = [];
+  arr.forEach(el => {
+    if (typeof(el) != "string"){
+      noStringsArr.push(el);
+    }
+  });
+  return noStringsArr;
+}
+
+// Test
+console.log(filter_list(['1',2,'3']));
