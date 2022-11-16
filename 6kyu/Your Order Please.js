036e71579411ab774bc8361274974e22263fa5fd
Kedus Meheretu <kmeheretu@gmail.com>
2022-11-16 16:44:52 -0500

    first commit

---
+++ 6kyu/Your Order Please.js
@@ -0,0 +1,8 @@
+function order(words){
+  return words.split(' ').sort(function(a, b){
+      return a.match(/\d/) - b.match(/\d/);
+   }).join(' ');
+}  
+
+console.log(order("is2 Thi1s T4est 3a"))
+
